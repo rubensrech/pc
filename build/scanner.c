5ee9a218e103e13c60b0985b00a4102af592d7b8
Rubens Rech <rubensch@icloud.com>
2018-03-09 14:13:02 -0300

    Matches all reserved words

--- build/scanner.c
+++ build/scanner.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,9 +361,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[19] =
     {   0,
-        0,    0,    4,    3,    1,    3,    0,    2,    0
+        0,    0,    6,    5,    1,    5,    5,    5,    0,    0,
+        0,    0,    0,    2,    4,    0,    3,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -377,10 +378,10 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    3,    4,    1,    1,
 
-        1,    1,    1,    1,    3,    1,    1,    1,    1,    4,
-        1,    1,    1,    1,    1,    5,    1,    1,    1,    1,
+        1,    5,    1,    1,    6,    1,    1,    7,    1,    8,
+        9,    1,    1,    1,    1,   10,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,33 +399,37 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[6] =
+static const YY_CHAR yy_meta[11] =
     {   0,
-        1,    1,    1,    2,    3
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[13] =
+static const flex_int16_t yy_base[20] =
     {   0,
-        0,    2,   11,   12,   12,    0,    0,   12,   12,    5,
-        8,    6
+        0,    5,   21,   22,   22,   11,   12,   10,    8,    7,
+        5,    7,   10,   22,   22,    2,   22,   22,    0
     } ;
 
-static const flex_int16_t yy_def[13] =
+static const flex_int16_t yy_def[20] =
     {   0,
-       10,   10,    9,    9,    9,   11,   12,    9,    0,    9,
-        9,    9
+       19,   19,   18,   18,   18,   18,   18,   18,   18,   18,
+       18,   18,   18,   18,   18,   18,   18,    0,   18
     } ;
 
-static const flex_int16_t yy_nxt[18] =
+static const flex_int16_t yy_nxt[33] =
     {   0,
-        9,    5,    6,    5,    6,    4,    4,    4,    8,    7,
-        9,    3,    9,    9,    9,    9,    9
+        4,    5,   18,    6,    7,    8,    5,   18,    6,    7,
+        8,   17,   16,   15,   14,   13,   12,   11,   10,    9,
+       18,    3,   18,   18,   18,   18,   18,   18,   18,   18,
+       18,   18
     } ;
 
-static const flex_int16_t yy_chk[18] =
+static const flex_int16_t yy_chk[33] =
     {   0,
-        0,    1,    1,    2,    2,   10,   10,   10,   12,   11,
-        3,    9,    9,    9,    9,    9,    9
+       19,    1,    0,    1,    1,    1,    2,    0,    2,    2,
+        2,   16,   13,   12,   11,   10,    9,    8,    7,    6,
+        3,   18,   18,   18,   18,   18,   18,   18,   18,   18,
+       18,   18
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -450,8 +455,8 @@ char *yytext;
 
 int num_lines = 1;
 
-#line 453 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
-#line 454 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
+#line 458 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
+#line 459 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
 
 #define INITIAL 0
 
@@ -668,10 +673,10 @@ YY_DECL
 		}
 
 	{
-#line 11 "scanner.l"
+#line 13 "scanner.l"
 
 
-#line 674 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
+#line 679 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -698,13 +703,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 19 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 12 );
+		while ( yy_base[yy_current_state] != 22 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -731,20 +736,30 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 /* rule 1 can match eol */
 YY_RULE_SETUP
-#line 13 "scanner.l"
+#line 15 "scanner.l"
 ++num_lines;
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 14 "scanner.l"
-return TK_PR_INT; 
+#line 17 "scanner.l"
+return TK_PR_INT;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 16 "scanner.l"
+#line 18 "scanner.l"
+return TK_PR_FLOAT;
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 19 "scanner.l"
+return TK_PR_BOOL;
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 22 "scanner.l"
 ECHO;
 	YY_BREAK
-#line 747 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
+#line 762 "/Users/rubensrechjunior/Developer/pc/build/scanner.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1041,7 +1056,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 19 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1069,11 +1084,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 19 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 18);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1749,6 +1764,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 16 "scanner.l"
+#line 22 "scanner.l"
 
 

32d34a63fd20029ad54dc26d7f79b1ad610b6847
Rubens Rech <rubensch@icloud.com>
2018-04-04 22:11:05 -0300

    Add more information to symbol table

--- include/cc_misc.h
+++ include/cc_misc.h
@@ -2,12 +2,25 @@
 #define __MISC_H
 #include <stdio.h>
 
+typedef struct hashEntry {
+    int line;
+    int type;
+    union {
+      int intVal;
+      float floatVal;
+      char charVal;
+      int boolVal;
+      char *strVal;  
+    } value;
+} HashValue;
+
 int getLineNumber (void);
 void yyerror (char const *mensagem);
 void main_init (int argc, char **argv);
 void main_finalize (void);
 
 void comp_print_table (void);
-void addSymbolsTable();
+void addSymbolsTable(int tokenType);
+void debugPrintHashValue(HashValue *value);
 
 #endif
--- src/cc_misc.c
+++ src/cc_misc.c
@@ -4,6 +4,8 @@
 #include "cc_dict.h"
 #include "main.h"
 
+#define MAX_HASH_KEY_SIZE 1000
+
 extern int num_lines;
 extern char *yytext;
 
@@ -39,7 +41,9 @@ void main_finalize (void)
   //implemente esta função com rotinas de inicialização, se necessário
   int i;
   struct comp_dict_item *entry, *next;
-  void *value;
+  HashValue *value;
+
+  // comp_print_table();
 
   // Free symbols table
   for (i = 0; i < symbolsTable->size; i++) {
@@ -62,13 +66,16 @@ void comp_print_table (void)
 
   int i;
   struct comp_dict_item *entry, *tmp;
-  int *line;
+  HashValue *value;
 
   for (i = 0; i < symbolsTable->size; i++) {
     entry = symbolsTable->data[i];
       while (entry != NULL) {
-        line = entry->value;
-        cc_dict_etapa_1_print_entrada(entry->key, *line);
+        value = entry->value;
+
+        //debugPrintHashValue(value);
+        cc_dict_etapa_1_print_entrada(entry->key, value->line);
+        
         entry = entry->next;
       }
   }
@@ -77,7 +84,6 @@ void comp_print_table (void)
 void removeQuotes(char *token) {
   int i;
   if (token[0] == '\'' || token[0] == '"') {
-    // Remove literal strings/chars quotes
     token[strlen(token)-1] = '\0';
     for (i = 0; i < strlen(token); i++) {
       token[i] = token[i+1];
@@ -85,14 +91,57 @@ void removeQuotes(char *token) {
   }
 }
 
-void addSymbolsTable() {
-  char *token = strdup(yytext);
-  int *line = (int *)malloc(sizeof(int));
+void addSymbolsTable(int tokenType) {
+  char *token;
+  char key[MAX_HASH_KEY_SIZE+1];
+  HashValue *entryValue = malloc(sizeof(struct hashEntry)); 
 
-  removeQuotes(token);
+  token = strdup(yytext);
 
-  (*line) = num_lines;
+  // Generate hash table key (TOKEN $$ TYPE)
+  snprintf(key, MAX_HASH_KEY_SIZE, "%s $$ %d", token, tokenType);
+  
+  entryValue->line = num_lines;
+  entryValue->type = tokenType;
 
-  dict_put(symbolsTable, token, line);
-  free(token); // token is duplicated (strdup) inside dict_put function
+  // Get token real value
+  switch (tokenType) {
+    case POA_LIT_STRING:
+      removeQuotes(token);
+      entryValue->value.strVal = token;
+      break;
+    case POA_LIT_CHAR:
+      removeQuotes(token);
+      entryValue->value.charVal = token[0];
+      break;
+    case POA_IDENT:
+      entryValue->value.strVal = token;
+      break;
+    case POA_LIT_BOOL:
+      entryValue->value.boolVal = !strcmp(token, "true");
+      break;
+    case POA_LIT_FLOAT:
+      entryValue->value.floatVal = atof(token);
+      break;
+    case POA_LIT_INT:
+      entryValue->value.intVal = atoi(token);
+      break;
+  }
+
+  // Key is duplicated (strdup) inside dict_put function
+  dict_put(symbolsTable, key, entryValue);
+}
+
+void debugPrintHashValue(HashValue *value) {
+    printf(">> ---------------\n");
+    printf(">> Line: %d\n", value->line);
+    printf(">> Type: %d\n", value->type);
+    switch (value->type) {
+      case POA_LIT_STRING: printf(">> Value: %s\n", value->value.strVal); break;
+      case POA_LIT_CHAR: printf(">> Value: %c\n", value->value.charVal); break;
+      case POA_IDENT: printf(">> Value: %s\n", value->value.strVal); break;
+      case POA_LIT_BOOL: printf(">> Value: %d\n", value->value.boolVal); break;
+      case POA_LIT_FLOAT: printf(">> Value: %.2f\n", value->value.floatVal); break;
+      case POA_LIT_INT: printf(">> Value: %d\n", value->value.intVal); break;
+    }
 }
